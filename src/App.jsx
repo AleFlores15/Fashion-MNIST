2c5a01abc97b342d42549773789d9500e1b16bce
unknown <alejandro.flores.a@ucb.edu.bo>
2023-06-13 12:09:17 -0400

    xd

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import React, { useEffect, useRef } from 'react';
+import * as tf from '@tensorflow/tfjs';
 function App() {
-  const [count, setCount] = useState(0)
+  const modelRef = useRef(null);
+
+  useEffect(() => {
+    // Cargar el modelo al montar el componente
+    async function loadModel() {
+      const model = await tf.loadLayersModel('./../model.json');
+      modelRef.current = model;
+    }
+    loadModel();
+
+    // Limpiar el modelo al desmontar el componente
+    return () => {
+      if (modelRef.current) {
+        modelRef.current.dispose();
+      }
+    };
+  }, []);
+
+  const handlePrediction = async () => {
+    const img = document.getElementById('image-element');
+    const processedImg = tf.browser.fromPixels(img).toFloat().expandDims();
+
+    if (modelRef.current) {
+      const prediction = modelRef.current.predict(processedImg);
+      const result = await prediction.data();
+
+      // Mostrar los resultados
+      console.log(result);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <img id="image-element" src="assets/00008.jpg" alt="Input" />
+      <button onClick={handlePrediction}>Realizar predicción</button>
+    </div>
+  );
 }
 
 export default App
